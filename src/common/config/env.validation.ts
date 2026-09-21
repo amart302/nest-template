b9a1e675ffc8cbcccf057961c417a229d933361c
amart302 <amartazanov892@gmail.com>
2026-09-22 00:31:00 +0300

    chore: убрал неиспользуемую JWT-конфигурацию из шаблона

--- src/common/config/env.validation.ts
+++ src/common/config/env.validation.ts
@@ -5,8 +5,5 @@ export const envValidationSchema = Joi.object({
 
   DATABASE_URL: Joi.string().uri().required(),
 
-  JWT_ACCESS_SECRET: Joi.string().min(16).required(),
-  JWT_ACCESS_EXPIRES_IN: Joi.number().required(),
-
   FRONTEND_URL: Joi.string().uri().required(),
 });
